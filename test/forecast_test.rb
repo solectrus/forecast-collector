0d3daa2f655a6d2c7f28ff705b3c59b23a1306c3
Georg Ledermann <georg@ledermann.dev>
2023-09-26 12:40:15 +0200

    Test accumulation

--- test/forecast_test.rb
+++ test/forecast_test.rb
@@ -40,4 +40,52 @@ class ForecastTest < Minitest::Test
     assert_match(/Too Many Requests/, out)
     assert_empty(err)
   end
+
+  def test_accumulation
+    base_plane = {
+      latitude: '51.13',
+      longitude: '10.42',
+      azimuth: '0',
+      declination: '30',
+      damping_morning: '0',
+      damping_evening: '0',
+    }
+
+    out, err =
+      capture_io do
+        # Two planes with 4 and 5 kwp
+        multi_plane_result =
+          VCR.use_cassettes([{ name: 'forecast_solar_multiplane' }]) do
+            Forecast.new(
+              config:
+                Config.from_env(
+                  forecast_configurations: [
+                    base_plane.merge(kwp: '4'),
+                    base_plane.merge(kwp: '5'),
+                  ],
+                ),
+            ).fetch_data
+          end
+
+        # Single plane with 9 kwp
+        single_plane_result =
+          VCR.use_cassettes([{ name: 'forecast_solar_singleplane' }]) do
+            Forecast.new(
+              config:
+                Config.from_env(
+                  forecast_configurations: [base_plane.merge(kwp: '9')],
+                ),
+            ).fetch_data
+          end
+
+        # Result should be the same: Same keys, same values (accept small delta)
+        assert_equal(multi_plane_result.keys, single_plane_result.keys)
+        single_plane_result.each do |key, value|
+          assert_in_delta(value, multi_plane_result[key], 2)
+        end
+      end
+
+    assert_match(/OK/, out)
+    assert_empty(err)
+  end
 end
