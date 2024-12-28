d808037275825d25c0a7b6b0352ab442ab0935e7
Georg Ledermann <georg@ledermann.dev>
2024-12-28 13:02:36 +0100

    ⬆️ Ruby 3.4.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,6 @@ require 'minitest/autorun'
 require 'climate_control'
 
 require File.expand_path './support/vcr_setup.rb', __dir__
+
+# Silence deprecation warnings caused by the `influxdb-client` gem
+Warning[:deprecated] = false
